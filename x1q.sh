9e33710f71581783f2253752bb7a44ddcb9cd652
Jisan <166594898+SameerAlSahab@users.noreply.github.com>
2025-06-02 08:47:01 +0600

    Update x1q.sh

--- x1q.sh
+++ x1q.sh
@@ -3,7 +3,7 @@
 KERNEL_DIR="$(pwd)"
 GCC_PATH="/usr/bin/"
 LLD_PATH="/usr/bin/"
-KERNEL_NAME="not_kernel-"
+KERNEL_NAME="hazardkernel"
 MAKE="./makeparallel" 
 KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
@@ -17,28 +17,28 @@ cd "$KERNEL_DIR" || exit 1
 
 MAKE_OPT+=(CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-)
 
-rm -rf /home/skye/bomb/out/arch/arm64/boot/Image
-rm -rf /home/skye/bomb/AnyKernel3/dtb
-rm -rf /home/skye/bomb/dtbo.img
+rm -rf /home/kona/kernel/out/arch/arm64/boot/Image
+rm -rf /home/kona/kernel/AnyKernel3/dtb
+rm -rf /home/kona/kernel/dtbo.img
 rm -rf .version
 rm -rf .local
-#make O=/home/skye/bomb/out clean
-make O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" vendor/kona-not_defconfig vendor/samsung/x1q.config vendor/debugfs.config
+#make O=/home/kona/kernel/out clean
+make O=/home/kona/kernel/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" vendor/kona-not_defconfig vendor/samsung/x1q.config vendor/debugfs.config
 
 echo "*****************************************"
 echo "*****************************************"
 
-make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}"  dtbs
-DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
+make -j12 O=/home/kona/kernel/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}"  dtbs
+DTB_OUT="/home/kona/kernel/out/arch/arm64/boot/dts/vendor/qcom"
 cat $DTB_OUT/*.dtb > AnyKernel3/dtb
 
-#make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
-DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
-#cp $DTBO_OUT/dtbo.img /home/skye/bomb/dtbo.img
-make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" Image
-IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
+#make -j12 O=/home/kona/kernel/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
+DTBO_OUT="/home/kona/kernel/out/arch/arm64/boot"
+#cp $DTBO_OUT/dtbo.img /home/kona/kernel/dtbo.img
+make -j12 O=/home/kona/kernel/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" Image
+IMAGE="/home/kona/kernel/out/arch/arm64/boot/Image"
 echo "**Build outputs**"
-ls /home/skye/bomb/out/arch/arm64/boot
+ls /home/kona/kernel/out/arch/arm64/boot
 echo "**Build outputs**"
 cp $IMAGE AnyKernel3/Image
 
